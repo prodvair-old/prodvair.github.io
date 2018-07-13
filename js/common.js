e179e67fae7587ade6a19404df064bf2992a3d06
prodvair <33940999+prodvair@users.noreply.github.com>
2018-07-13 20:46:10 +0300

    New site

--- js/common.js
+++ js/common.js
@@ -1,63 +1,24 @@
-$(".bar").on('click', function (e) {
-	e.preventDefault();
-	$('.head-line').toggleClass("fixed container");
-	$('.mmenu').toggleClass("mmenu-active");
-	$('.logo').toggleClass("d-none");
-	$('.mlogo').toggleClass("d-none");
-	$('.bar').toggleClass("m-active");
-});
+$(function() {
 
-$(".menu a").on("click", function (e) {
-        e.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='"+id+"']").addClass('active');
-        if (($(window).width() < 992) && (!$(this).hasClass('footer__menu__ul__li__a'))) {
-			$('.head-line').toggleClass("fixed container");
-        	$('.mmenu').toggleClass("mmenu-active");
-        	$('.logo').toggleClass("d-none");
-			$('.mlogo').toggleClass("d-none");
-			$('.bar').toggleClass("m-active");
-        }
-        $('body,html').animate({scrollTop: top}, 1000);
-});
+	$('.header__bar__mmenu-burger').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('header__bar__mmenu-burger-active');
+		$('.header__bar__mmenu ul').toggleClass('active');
+	});
 
-$(window).scroll(function () {
-	scrollCheck();
-});
-
-function scrollCheck() {
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 50000,
+		step: 50,
+		values: [ 5000, 45000 ],
+		slide: function( event, ui ) {
+		$( ".top__filter-null" ).html(ui.values[ 0 ]);
+		$( ".top__filter-all" ).html(ui.values[ 1 ]);
+		}
+	});
+	$( ".top__filter-null" ).html( $( "#slider-range" ).slider( "values", 0 ) );
+	$( ".top__filter-all" ).html( $( "#slider-range" ).slider( "values", 1 ) );
 	
-	var home = $('#home').position(),
-        info = $('#info').position(),
-        services = $('#services').position(),
-        adventages = $('#adventages').position(),
-        contacts = $('#contacts').position(),
-        c = 0;
 
-    if ($(window).width() > 1200) {
-    	c = 50;
-    }
-
-    if ($(this).scrollTop() >= home.top) {
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='#home']").addClass('active');
-    }
-    if ($(this).scrollTop() >= info.top) {
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='#info']").addClass('active');
-    }
-    if ($(this).scrollTop() >= services.top) {
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='#services']").addClass('active');
-    }
-    if ($(this).scrollTop() >= adventages.top) {
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='#adventages']").addClass('active');
-    }
-    if ($(this).scrollTop() >= (contacts.top - c)) {
-        $(".menu a").removeClass('active');
-        $(".menu a[href^='#contacts']").addClass('active');
-    }
-}
+});
