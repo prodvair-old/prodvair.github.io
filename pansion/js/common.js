832a50bf238b663cb8178e8cbee7271dc54ad3cf
prodvair <33940999+prodvair@users.noreply.github.com>
2018-10-15 23:50:50 +0300

    new

--- pansion/js/common.js
+++ pansion/js/common.js
@@ -6,4 +6,38 @@ $(function() {
 		$('.header__mmenu').toggleClass('active');
 	});
 
+	$('.gallery__content').slick({
+		infinite: true,
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToScroll: 2,
+					slidesToShow: 2
+				}
+			},
+			{
+			  breakpoint: 768,
+			  settings: {
+				arrows: false,
+				slidesToScroll: 2,
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				arrows: false,
+				slidesToScroll: 1,
+				slidesToShow: 1
+			  }
+			}
+		  ]
+	});
+
 });
